3b4ba3a41a4e65d1183f0ebd0fbafbf868bdc96b
DM <michaeldm034@gmail.com>
2023-06-16 22:12:19 +0300

    Sprint10

--- script.bat
+++ script.bat
@@ -1,14 +1,14 @@
-cd framework/src
+cd Framework/src
 javac -parameters -d . *.java
 jar -cvf ../../etu1808.jar etu1808
-copy ../../etu1808.jar ../../test-framework/WEB-INF/lib/
+copy ../../etu1808.jar ../../TestFramework/WEB-INF/lib/
 
 set CLASSPATH=.;C:\Program Files\Apache Software Foundation\Tomcat 8.5_Tomcat8Oni\webapps\etu1808.jar
 
-cd ../../test-framework/WEB-INF/classes
+cd ../../TestFramework/WEB-INF/classes
 javac -parameters -d . *.java
 cd ../../
-jar -cvf ../test-framework.war .
+jar -cvf ../TestFramework.war .
 
 cd ../
 pause
